926bcb1109b5c59dc8d38fd89667ff1613af9f08
Igor Barreto <igorbarreto1993@hotmail.com>
2020-05-12 22:14:16 -0300

    finalizando o desafio 1 gostack11

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid} = require('uuidv4')
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,46 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body
+  const repository = {id:uuid(),title,url,techs,likes:0}
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const {title, url, techs} =request.body
+  const Indexrepository = repositories.findIndex(repository=> repository.id ===id)
+  if(Indexrepository<0){
+    return response.status(400).json()
+  }
+  const repository = repositories[Indexrepository]
+  repositories[Indexrepository] = {id:repository.id,title, url,techs,likes:repository.likes}
+  return response.json(repositories[Indexrepository])
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id}=request.params
+  const Indexrepository = repositories.findIndex(repository => repository.id ===id)
+  if(Indexrepository<0){
+    return response.status(400).json({error:'Repository not found'})
+  }
+  repositories.splice(Indexrepository,1)
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} =request.params
+  const findIndex = repositories.findIndex(repository=>repository.id === id)
+  if(findIndex<0){
+    return response.status(400).json({error:'Repository does not exist'})
+  }
+  repositories[findIndex].likes++
+  return response.json(repositories[findIndex])
 });
 
 module.exports = app;
